851229afba017e42f36f262f4fb104ab0d90d3a8
Justin <29545213+Scrawk@users.noreply.github.com>
2022-06-02 21:09:46 +0800

    update

--- CGALDotNetConsole/Program.cs
+++ CGALDotNetConsole/Program.cs
@@ -31,37 +31,6 @@ namespace CGALDotNetConsole
         static void Main(string[] args)
         {
 
-            var indices = new PolygonalIndices();
-
-            indices.quads = new int[]
-            {
-                3, 2, 1, 0
-            };
-
-            var points = new Point3d[]
-            {
-                new Point3d(0, 0, 0),
-                new Point3d(1, 0, 0),
-                new Point3d(1, 1, 0),
-                new Point3d(0, 1, 0)
-            };
-
-            var mesh = SurfaceMeshFactory<EEK>.CreateUVSphere();
-            //mesh.Print();
-
-            var distances = new List<double>();
-
-            var timer = new Timer();
-            timer.Start();
-
-           HeatMethod<EEK>.Instance.EstimateGeodesicDistances(mesh, 0, distances);
-
-            timer.Stop();
-
-            Console.WriteLine("Time = " + timer.ElapsedMilliseconds);
-
-            //foreach(var d in distances)
-            //    Console.WriteLine(d);
 
         }
 
